0ee44bd9f15a6e7a536c8d1b01c42cea92418559
Christopher M. Neill <chris@runnable.com>
2016-10-11 17:50:16 -0700

    errors only

--- src/start.sh
+++ src/start.sh
@@ -9,5 +9,7 @@ service nginx start
 # Start the varnish daemon
 varnishd -f /etc/varnish/default.vcl \
   -s malloc,${VARNISH_MALLOC} \
-  -a 0.0.0.0:${VARNISH_PORT} \
-  -F 
+  -a 0.0.0.0:${VARNISH_PORT} 
+
+# Show only Varnish errors
+varnishlog -X "Error"
